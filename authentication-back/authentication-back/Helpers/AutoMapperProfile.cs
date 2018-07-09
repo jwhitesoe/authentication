1c2d968a00ed0703c6aa707230a1cfdb7c93b7d6
Unknown <jeremy.white@soeidental.com>
2018-07-09 16:03:57 +1200

    First checkin of working back end code

---
+++ authentication-back/authentication-back/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using WebApi.Entities;
+using WebApi.Dtos;
+
+
+namespace WebApi.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
